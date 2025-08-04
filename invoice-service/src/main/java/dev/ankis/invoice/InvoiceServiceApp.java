3abd927488767c4c1ac69abec4f6a6a789b43893
Ankit Sood - a0s0f6i <soodankit1993@gmail.com>
2025-08-05 00:38:32 +0530

    Initial Commit for invoice service.

---
+++ invoice-service/src/main/java/dev/ankis/invoice/InvoiceServiceApp.java
@@ -0,0 +1,11 @@
+package dev.ankis.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceServiceApp.class, args);
+    }
+}

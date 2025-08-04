abc6d357bbd26f671425caf9d39f831313e0cbb2
Ankit Sood - a0s0f6i <soodankit1993@gmail.com>
2025-08-05 00:40:59 +0530

    Initial Commit for Vendor Service.

---
+++ vendor-service/src/main/java/dev/ankis/vendors/VendorServiceApp.java
@@ -0,0 +1,11 @@
+package dev.ankis.vendors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(VendorServiceApp.class, args);
+    }
+}

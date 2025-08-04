317600f1104e86210aafd3e357d5cae372c81433
Ankit Sood - a0s0f6i <soodankit1993@gmail.com>
2025-08-05 00:40:17 +0530

    Initial Commit for Receiving Service.

---
+++ receiving-service/src/main/java/dev/ankis/receivings/ReceivingServiceApp.java
@@ -0,0 +1,11 @@
+package dev.ankis.receivings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceivingServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ReceivingServiceApp.class, args);
+    }
+}

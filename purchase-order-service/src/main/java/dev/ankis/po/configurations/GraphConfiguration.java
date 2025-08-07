b0a4aaee180e9f016ab3f1d3b8dbf0427d80ac8c
Ankit Sood - a0s0f6i <soodankit1993@gmail.com>
2025-08-07 22:51:33 +0530

    Adding support for entities queries.

---
+++ purchase-order-service/src/main/java/dev/ankis/po/configurations/GraphConfiguration.java
@@ -0,0 +1,19 @@
+package dev.ankis.po.configurations;
+
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.data.federation.FederationSchemaFactory;
+
+@Configuration
+public class GraphConfiguration {
+    @Bean
+    public GraphQlSourceBuilderCustomizer customizer(FederationSchemaFactory factory) {
+        return builder -> builder.schemaFactory(factory::createGraphQLSchema);
+    }
+
+    @Bean
+    FederationSchemaFactory federationSchemaFactory() {
+        return new FederationSchemaFactory();
+    }
+}
--- purchase-order-service/src/main/java/dev/ankis/po/controller/POController.java
+++ purchase-order-service/src/main/java/dev/ankis/po/controller/POController.java
@@ -3,6 +3,7 @@ package dev.ankis.po.controller;
 import dev.ankis.po.models.PurchaseOrder;
 import dev.ankis.po.models.PurchaseOrderLine;
 import dev.ankis.po.services.PurchaseOrderService;
+import org.springframework.graphql.data.federation.EntityMapping;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -18,6 +19,13 @@ public class POController {
     public POController(PurchaseOrderService purchaseOrderService) {
         this.purchaseOrderService = purchaseOrderService;
     }
+
+    @EntityMapping("PurchaseOrder")
+    public PurchaseOrder find(@Argument Long purchaseOrderNumber) {
+        List<PurchaseOrder> purchaseOrders = purchaseOrderService.getPoDetailsByPoNumber(purchaseOrderNumber);
+        return purchaseOrders.stream().findFirst().orElse(null);
+    }
+
 
     @QueryMapping("purchaseOrderWithPONumber")
     public List<PurchaseOrder> findPurchaseOrderByPONumber(@Argument Long purchaseOrderNumber) {
